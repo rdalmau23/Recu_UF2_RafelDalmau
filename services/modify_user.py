f76c7c5278a490fe3b885f5e734fcf7f7583a599
rdalmau23 <rdalmau23@ilg.cat>
2025-06-02 16:43:35 +0200

    exercici5 new changes

--- services/modify_user.py
+++ services/modify_user.py
@@ -1,13 +1,11 @@
 from database import connect_db
 
-from user import users_schema
-
 conn = connect_db.connection_db()
 
 def modify_user():
     cursor = conn.cursor()
     sql= """
-    UDATE SET user_lastname = $s,
+    UDATE SET user_lastname = $s, user_direction WHER user_id = $s
     """
     values = (lastname, direction )
 
